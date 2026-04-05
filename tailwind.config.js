0e563b442064fd39f9bb14c9684b0751a547949b
ananamartin85-commits <ananamartin85@gmail.com>
2026-04-05 12:03:13 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // <-- Esto le dice "buscá clases en toda la carpeta src"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
