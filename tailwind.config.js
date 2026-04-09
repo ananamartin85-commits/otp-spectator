da4d2c53f7ec9b1e2299e9423d7ce57c3a13bed9
ananamartin85-commits <ananamartin85@gmail.com>
2026-04-08 21:15:19 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // <-- Esto le dice "buscá clases en toda la carpeta src"
+    "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
     extend: {},
